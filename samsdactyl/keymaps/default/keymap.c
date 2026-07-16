857585f4b5a1e8334b9f7641987f160638da3eb8
Samuele Panzeri <samuele.panzeri@gmail.com>
2026-07-16 23:18:32 +0200

    Reverse space and delete so it makes more sense and feels a bit nicer to use

--- samsdactyl/keymaps/default/keymap.c
+++ samsdactyl/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
         KC_LGUI, KC_PAUS, KC_PGUP, KC_PGDN,                                                KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
-                                            KC_SPC,  KC_DEL,             KC_ENT,  KC_BSPC,
+                                            KC_DEL,  KC_SPC,             KC_ENT,  KC_BSPC,
                                             KC_LCTL, KC_LBRC,            KC_RBRC, KC_HOME,
                                             KC_LALT, RAISE,              LOWER,   KC_END
     ),
